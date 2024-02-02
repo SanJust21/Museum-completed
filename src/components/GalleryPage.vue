326b59c4316c7cf1c331283c98a29976c4836afa
nibexx <ebin.ptf@gmail.com>
2024-02-02 12:16:28 +0530

    gallery again updated

--- src/components/GalleryPage.vue
+++ src/components/GalleryPage.vue
@@ -93,7 +93,7 @@ export default {
         gallery: [
           {
             image: require("@/assets/gallery2.jpg"),
-            name: "KERALA HERITAGE MUSEUM <br> Handpainted Art Painting",
+            name: "Kerala heritage museum  ",
             description:
               "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
             show: false, // Add show property to each gallery item
@@ -128,7 +128,14 @@ export default {
           },
           {
             image: require("@/assets/gallery8.jpeg"),
-            name: "CULTURAL HISTORICAL ARCHITECTURAL MIRACLES",
+            name: "Cultural musical museum",
+            description:
+              "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
+            show: false, // Add show property to each gallery item
+          },
+          {
+            image: require("@/assets/gallery6.jpg"),
+            name: "Travancore Painters",
             description:
               "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
             show: false, // Add show property to each gallery item
