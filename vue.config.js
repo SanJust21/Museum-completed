24f3895cdb683aa8802eb8985efd725e1328327f
Rechzz <reshmareghu.ptf@gmail.com>
2024-05-07 11:24:27 +0530

    server

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    disableHostCheck: true
-  },
+  allowedHosts: [
+      'localhost',
+      '.aksharamuseum.com' 
+    ],
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
